858db0589ebb1b286c893a24498f2e87994a9f86
Leandro Ramos <leandroramos@usp.br>
2018-04-20 11:47:52 -0300

    Model Livro criado e migrado

---
+++ app/Http/Controllers/LivroController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Livro;
+use Illuminate\Http\Request;
+
+class LivroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Livro  $livro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Livro $livro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Livro  $livro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Livro $livro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Livro  $livro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Livro $livro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Livro  $livro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Livro $livro)
+    {
+        //
+    }
+}
